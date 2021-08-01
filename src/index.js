e000c4fc903bd7226f543f9ec2c5b459766bc237
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-01 14:49:27 +0200

    Add items list - props and destructuring

--- src/index.js
+++ src/index.js
@@ -4,56 +4,46 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-const firstBook = {
-  title: "The Stranger",
-  author: "Albert Camus",
-  image:
-    "https://images-na.ssl-images-amazon.com/images/I/51L54hW47kS._SX322_BO1,204,203,200_.jpg",
-};
-const secondBook = {
-  title: "Anna Karenina",
-  author: "Leo Tolstoy",
-  image:
-    "https://images-na.ssl-images-amazon.com/images/I/51-rwApY85L._SX333_BO1,204,203,200_.jpg",
-};
+const books = [
+  {
+    id: 1,
+    title: "The Stranger",
+    author: "Albert Camus",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51L54hW47kS._SX322_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 2,
+    title: "Anna Karenina",
+    author: "Leo Tolstoy",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51-rwApY85L._SX333_BO1,204,203,200_.jpg",
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        image={firstBook.image}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-          voluptatum impedit expedita quae quidem voluptatem praesentium est
-          dolore eligendi animi!
-        </p>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        image={secondBook.image}
-      />
+      {books.map((book, index) => {
+        // const { id, title, author, image } = book;
+        //return <Book key={id} title={title} author={author} image={image} />;
+        // not good for use-cases where the list is dynamically changed - add elements, remove them
+        // return <Book key={index} book={book} />;
+        return <Book key={index} {...book} />;
+      })}
     </section>
   );
 };
 
-// must name it children
-
 // const Book = ({ title, author, image, children }) => {
 const Book = (props) => {
   console.log(props);
+  //const { title, author, image } = props;
+  // const { title, author, image } = props.book;
   const { title, author, image } = props;
   return (
     <article className="book">
-      <img
-        src={image}
-        alt="The Stranger - Alber Camus"
-        width="200"
-        height="300"
-      />
+      <img src={image} alt={title} width="200" height="300" />
       <h1>{title}</h1>
       {props.children}
       <h4
