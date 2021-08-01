44484064a5427d789f870e78b308fbf4101d56af
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-01 14:16:24 +0200

    Add component props

--- src/index.js
+++ src/index.js
@@ -4,47 +4,69 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
+const firstBook = {
+  title: "The Stranger",
+  author: "Albert Camus",
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/51L54hW47kS._SX322_BO1,204,203,200_.jpg",
+};
+const secondBook = {
+  title: "Anna Karenina",
+  author: "Leo Tolstoy",
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/51-rwApY85L._SX333_BO1,204,203,200_.jpg",
+};
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        image={firstBook.image}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+          voluptatum impedit expedita quae quidem voluptatem praesentium est
+          dolore eligendi animi!
+        </p>
+      </Book>
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        image={secondBook.image}
+      />
     </section>
   );
 };
 
-const Book = () => {
+// must name it children
+
+// const Book = ({ title, author, image, children }) => {
+const Book = (props) => {
+  console.log(props);
+  const { title, author, image } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src={image}
+        alt="The Stranger - Alber Camus"
+        width="200"
+        height="300"
+      />
+      <h1>{title}</h1>
+      {props.children}
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        {author}
+      </h4>
+      {/* {console.log(props)} */}
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/51L54hW47kS._SX322_BO1,204,203,200_.jpg"
-    alt="The Stranger - Alber Camus"
-    width="200"
-    height="300"
-  />
-);
+// {} -> for JS
+// {{}} -> for CSS
 
-const Title = () => <h1>The Stranger</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Albert Camus
-  </h4>
-);
 ReactDom.render(<BookList />, document.getElementById("root"));
