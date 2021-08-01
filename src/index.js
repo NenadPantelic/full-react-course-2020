678f78ccaabd7497b18abc4899994cf463d5b8a1
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-01 15:12:52 +0200

    Add event handler examples

--- src/index.js
+++ src/index.js
@@ -35,23 +35,44 @@ const BookList = () => {
   );
 };
 
+const clickHandler = (e) => {
+  console.log(e);
+  console.log(e.target);
+  console.log("click");
+};
+
+const complexExample = (author) => {
+  alert(`Author: ${author}`);
+};
+
 // const Book = ({ title, author, image, children }) => {
 const Book = (props) => {
   console.log(props);
   //const { title, author, image } = props;
   // const { title, author, image } = props.book;
+
   const { title, author, image } = props;
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={image} alt={title} width="200" height="300" />
       <h1>{title}</h1>
       {props.children}
-      <h4
-        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
-      >
+      <h4>
+        {/* style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }} */}
         {author}
       </h4>
-      {/* {console.log(props)} */}
+      <button type="button" onClick={clickHandler}>
+        Example
+      </button>
+
+      <button type="button" onClick={() => complexExample(author)}>
+        Another example
+      </button>
     </article>
   );
 };
