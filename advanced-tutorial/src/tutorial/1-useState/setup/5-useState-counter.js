ca8344c1388d200487767bb87c11048fcdbc104a
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-02 14:44:32 +0200

    Update counter exercise

--- advanced-tutorial/src/tutorial/1-useState/setup/5-useState-counter.js
+++ advanced-tutorial/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value+1) -> same as only one click
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
@@ -11,10 +19,18 @@ const UseStateCounter = () => {
           Increase
         </button>
         <button className="btn" onClick={() => setValue(value - 1)}>
-          Increase
+          Decrease
         </button>
         <button className="btn" onClick={() => setValue(0)}>
           Reset
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          Increase with delay
         </button>
       </section>
     </>
