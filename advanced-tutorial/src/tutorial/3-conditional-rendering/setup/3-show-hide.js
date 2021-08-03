f68dc89b99d08bd824ba4d5f037cc289caba7cb7
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-03 21:45:59 +0200

    Add show-hide exercise and controlled input example

--- advanced-tutorial/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ advanced-tutorial/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className="button" onClick={() => setShow(!show)}>
+        Show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
 };
 
+const Item = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const checkWidth = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    // we need cleanup function, because when we toggle the component
+    // we seek for re-render, so we use cleanup to have only one event listener
+    return () => {
+      window.removeEventListener("resize", checkWidth);
+    };
+  }, [width]);
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>Window</h1>
+      <h1>Width:{width}</h1>
+    </div>
+  );
+};
 export default ShowHide;
