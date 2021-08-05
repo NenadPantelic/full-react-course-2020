bfdcf2bbec42cd8cda9e63558038c33b65be8813
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-05 20:29:10 +0200

    Add prop drilling example

--- advanced-tutorial/src/App.js
+++ advanced-tutorial/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // index.js from any folder is the default entry-point
-import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 function App() {
   return (
     <div className="container">
--- advanced-tutorial/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ advanced-tutorial/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,54 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { data } from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
 
+// how to pass some property deep down the tree -> prop drilling
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <section>
+      <h3>Prop drilling</h3>
+      {/* functions can also be passed as property */}
+      <List people={people} removePerson={removePerson} />
+    </section>
+  );
+};
+const List = ({ people, removePerson }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <Person
+            key={person.id}
+            id={person.id}
+            name={person.name}
+            removePerson={removePerson}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const Person = ({ id, name, removePerson }) => {
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button
+        onClick={() => {
+          removePerson(id);
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
 };
 
 export default PropDrilling;
