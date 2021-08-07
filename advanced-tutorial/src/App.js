4d6b4d26228baf43e87388152a92f721ff4e82bc
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-07 21:37:51 +0200

    Add prop types example

--- advanced-tutorial/src/App.js
+++ advanced-tutorial/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // index.js from any folder is the default entry-point
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+import Setup from "./tutorial/10-prop-types/setup/";
 function App() {
   return (
     <div className="container">
--- advanced-tutorial/src/tutorial/10-prop-types/setup/Product.js
+++ advanced-tutorial/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
+const Product = ({ image, name, price }) => {
+  return (
+    <article className="product">
+      {/* without prop types */}
+      {/* <img src={image ? image.url : ""} alt={name} />
+      <h4>{name}</h4>
+      <p>${price || "N/A"}</p> */}
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+      {/* with prop types -> with default values*/}
+      <img src={image.url} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p>
+    </article>
+  );
+};
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 };
 
+Product.defaultProps = {
+  name: "N/A",
+  price: 3.99,
+  image: { url: defaultImage },
+};
 export default Product;
