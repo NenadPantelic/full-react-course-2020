d407334ea9673fbe463f0b9cc19eeb6ae678f13c
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-07 21:13:37 +0200

    Add custom hook example

--- advanced-tutorial/src/App.js
+++ advanced-tutorial/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // index.js from any folder is the default entry-point
-import Setup from "./tutorial/8-useContext/setup/1-context-api";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 function App() {
   return (
     <div className="container">
--- advanced-tutorial/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ advanced-tutorial/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+// naming convention -> it must be in the form useXXXXXX
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
 
-export const useFetch = () => {};
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+  return { loading, products };
+};
