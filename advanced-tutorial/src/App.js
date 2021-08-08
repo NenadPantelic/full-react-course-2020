ee29f54814598c22a9b8ee6d814497cdc1793916
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-08 12:18:11 +0200

    Add React Router examples

--- advanced-tutorial/src/App.js
+++ advanced-tutorial/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // index.js from any folder is the default entry-point
-import Setup from "./tutorial/10-prop-types/setup/";
+import Setup from "./tutorial/11-react-router/setup/";
 function App() {
   return (
     <div className="container">
--- advanced-tutorial/src/tutorial/11-react-router/setup/index.js
+++ advanced-tutorial/src/tutorial/11-react-router/setup/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React from "react";
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // pages
-import Home from './Home';
-import About from './About';
-import People from './People';
-import Error from './Error';
-import Person from './Person';
+import Home from "./Home";
+import About from "./About";
+import People from "./People";
+import Error from "./Error";
+import Person from "./Person";
 // navbar
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  // router would generally wrap App.js or index.js
+  // if the matches multiple routes, all will be displayed - without exact
+  return (
+    <Router>
+      <Navbar />
+      {/* just the first route that matches will be displayed */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/people">
+          <People />
+        </Route>
+        <Route path="/people/:id" children={<Person />}></Route>
+        {/* any route path will match */}
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
